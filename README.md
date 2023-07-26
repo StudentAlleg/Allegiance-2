85549fd997c806a3dc2f8af6e95825cb39b03a0a
StudentAlleg <studentalleg-pilot@yahoo.com>
2023-07-26 09:50:28 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+StudentAlleg
+This is a project primarily for learning the Godot game engine, by working on building Allegiance 2 in Godot. 
+This is based off of Microsoft Allegiance, which gives a lot of interesting problems and ideas with which to explore Godot with
+Quality of development may vary.
---
+++ ship.gd
@@ -0,0 +1,88 @@
+class_name Ship
+extends RigidBody3D
+
+var max_angular_velocity = 0.5
+
+var vRotation = Vector3.ZERO
+var vThrust = Vector3.ZERO
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	pass # Replace with function body.
+
+func update_thrust():
+	#TODO: this might be a slider to affect slide thrusting
+	var thrust_modifier = 1
+	
+	
+	#TODO: make this based off of direction looking (relative)
+	var new_thrust = Vector3.ZERO
+	
+	if Input.is_action_pressed("thrust_right"):
+		new_thrust.x += 1 * thrust_modifier
+	
+	if Input.is_action_pressed("thrust_left"):
+		new_thrust.x += -1 * thrust_modifier
+	
+	if Input.is_action_pressed("thrust_up"):
+		new_thrust.y += 1 * thrust_modifier
+		
+	if Input.is_action_pressed("thrust_up"):
+		new_thrust.y += 1 * thrust_modifier
+		
+	if Input.is_action_pressed("thrust_down"):
+		new_thrust.y += -1 * thrust_modifier
+		
+	if Input.is_action_pressed("thrust_forward"):
+		new_thrust.z += 1 * thrust_modifier
+		
+	if Input.is_action_pressed("thrust_backward"):
+		new_thrust.y += -1 * thrust_modifier
+	
+	if new_thrust != Vector3.ZERO:
+		new_thrust = new_thrust.normalized()
+	
+	return new_thrust
+
+func update_rotation(mouse_vector):
+	var vR = vRotation
+	
+	vR.x += mouse_vector.x/64
+	vR.y += mouse_vector.y/64
+	
+	var zRoll = 0
+	if Input.is_action_pressed("roll_left"):
+		zRoll += -1
+		
+	if Input.is_action_pressed("roll_right"):
+		zRoll += 1
+	
+	vR.z = zRoll
+	return vR.normalized()	
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	
+	pass
+	
+func _integrate_forces (state):
+	#TODO make a force matrix based off of keys held down
+	vThrust = update_thrust()
+	
+	#update the roll rotation
+	vRotation = update_rotation(Vector2.ZERO)
+	
+	state.apply_force(vThrust)
+	state.apply_torque(vRotation)
+	
+	#apply an upper limit to angluar_velocity
+	#this probably is not functional
+	if (state.get_angular_velocity().length() > max_angular_velocity):
+		state.set_angular_velocity(state.get_angular_velocity().normalized())
+	
+func _input(event): 
+	#handle mouse movement when flying ship
+	if event is InputEventMouseMotion:
+		vRotation = update_rotation(event.relative)
+
