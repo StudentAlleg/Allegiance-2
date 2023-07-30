dd8cbef4cf73bb5253dbe5fa2c97e53a7be77650
StudentAlleg <studentalleg-pilot@yahoo.com>
2023-07-29 21:00:14 -0700

    prepping to start igc like structure

---
+++ GLF.gd
@@ -0,0 +1,17 @@
+class GLF extends Node:
+#This is the base file that most Game Logic Objects will inherit
+#This is based off of igc.h from github.com/freeallegiance/allegiance/src/igc
+
+
+
+
+
+
+	# Called when the node enters the scene tree for the first time.
+	func _ready():
+		pass # Replace with function body.
+
+
+	# Called every frame. 'delta' is the elapsed time since the previous frame.
+	func _process(delta):
+		pass
